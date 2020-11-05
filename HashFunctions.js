ac7eb3ae1ace6024d47109126d8bf5dc97327380
Dimitri McDaniel <devebeme@gmail.com>
2020-11-05 07:23:59 -0500

    Update HashFunctions.js
    adding hash table and related method in which data can be set within said table

--- HashFunctions.js
+++ HashFunctions.js
@@ -1,10 +1,48 @@
-// Hash functions
+// :: creating a custom hash table using class :: 
 
-// pros of using Hash tables / functions :
+class HashTable {  
+    constructor(size){
+        // this will allow us to set the size when instantiating later
+        this.data = new Array(size);
+    }
 
-// 1. fast access of contents - run-time O(1) for all operations [ access, looklup, search , delete ]
+    // identifier named hash, or more fully 'hash key'
+    // similar to private props in other langs 
+    _hash(key) {
+        let hash = 0;
+        // as the hash table is filled with values their hashes are being abstracted 
+        for (let i=0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) %
+            this.data.length; // enforces that the output stays beneath the assigned size of 50
+        }
+        return hash;
+    }
+    set(key,value){
+        let address = this._hash(key);
+        // to avoid collision's which will mean the deleting of data
+        if (!this.data[address]){
+            this.data[address] = [];
+        }
+         this.data[address].push([key, value]);
+         return this.data
+    }
+    get(key){
+        let address = this._hash(key);
+        const currentBucket = this.data[address];   
+        if (currentBucket){
+            for (let i=0; i < currentBucket.length; i++)
+            {
+                if(currentBucket[i][0] === key) {
+                    return currentBucket[i][1]
+                }    
+            }
+        }
+        return undefined
+    }
+}
 
+// setting size of Hash table to 50
+const myHashTable = new HashTable(50);
+myHashTable.set('oranges', 100);
+myHashTable.set('apples', 54);
 
-// cons of using Hash tables:
-
-// 1. Collision : when we insert something into an hash table - the hash function does not evenly distribute it throught the length of the data structure
